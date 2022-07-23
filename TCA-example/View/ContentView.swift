fcd26a5f7a52ee124d7819893bcbfa3a1cb7ccdb
narumichikubo <narumichi7373@gmail.com>
2022-07-23 18:55:45 +0900

    chanded directory structure

---
+++ TCA-example/View/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  TCA-example
+//
+//  Created by Narumichi Kubo on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+
