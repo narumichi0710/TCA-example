22a25396c8a5850ec395dd2a2c0be84a15063174
narumichikubo <narumichi7373@gmail.com>
2022-07-23 18:15:18 +0900

    init

---
+++ TCA-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCA-example
+//
+//  Created by Narumichi Kubo on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA-example/TCA_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_exampleApp.swift
+//  TCA-example
+//
+//  Created by Narumichi Kubo on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
