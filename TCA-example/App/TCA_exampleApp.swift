8e9d186ef7c1c9bca1a144d6576e6199ae5d814a
narumichikubo <narumichi7373@gmail.com>
2022-08-13 19:01:11 +0900

    update main view

---
+++ TCA-example/App/TCA_exampleApp.swift
@@ -0,0 +1,44 @@
+//
+//  TCA_exampleApp.swift
+//  TCA-example
+//
+//  Created by Narumichi Kubo on 2022/07/23.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+
+@main
+struct TCA_exampleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView(store: appDelegate.store)
+        }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let store = Store(
+        initialState: AppState(),
+        reducer: appReducer,
+        environment: AppEnvironment(
+            usersClient: .live
+        )
+    )
+    
+    lazy var viewStore = ViewStore(store)
+
+    func application(
+        _ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        // アプリ起動処理
+        viewStore.send(.appDelegate(.onLaunchFinish))
+        
+        print("yobaremasita")
+        return true
+    }
+}
